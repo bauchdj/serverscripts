852c01f048eb02c98d25d004a6f4b3776c5de06a
David Bauch <64243465+bauchdj@users.noreply.github.com>
2023-06-06 03:55:53 -0600

    Update chatgpt-proxy.js
    The first version was good but the websocket upgrade requires an event listener so you can get the req, socket, head parameters. ChatGPT made my code better

--- chatgpt-proxy.js
+++ chatgpt-proxy.js
@@ -2,63 +2,58 @@ const https = require('https');
 const httpProxy = require('http-proxy');
 const fs = require('fs');
 
+const proxyPort = 443;
+const targetHost = '127.0.0.1';
+
 // Create a proxy server instance
 const proxy = httpProxy.createProxyServer({
   ws: true, // Enable WebSocket proxying
 });
 
-// Read SSL certificate and key files
-const options = {
-  cert: fs.readFileSync('/path/to/certificate.crt'),
-  key: fs.readFileSync('/path/to/privatekey.key'),
+proxy.on('error', (err, req, res) => { // Error event listener
+  console.error('Proxy error:', err);
+  res.writeHead(500, { 'Content-Type': 'text/plain' });
+  res.end('Something went wrong.');
+});
+
+const options = { // Read SSL certificate and key files
+  cert: fs.readFileSync('/etc/letsencrypt/live/f.talkofchrist.org/fullchain.pem'),
+  key: fs.readFileSync('/etc/letsencrypt/live/f.talkofchrist.org/privkey.pem'),
 };
+
+function getTargetPort(req) { // Gets port based on subdomain
+  const subdomain = req.headers.host.split('.')[0];
+  switch (subdomain) {
+    case 'jf':
+      return 8096;
+    case 'f':
+      return 8080;
+    case 'drop':
+      return 3030;
+    case 'call':
+    case 'meet':
+      return 3000;
+    default:
+      return null;
+  }
+}
 
 // Create an HTTPS server
 const server = https.createServer(options, (req, res) => {
-  const subdomain = getSubdomain(req);
-
-  // Set the target port based on the subdomain
-  let targetPort;
-  switch (subdomain) {
-    case 'app1':
-      targetPort = 8081;
-      break;
-    case 'app2':
-      targetPort = 8082;
-      break;
-    default:
-      targetPort = 8080;
-  }
-
-  // Construct the target URL using the subdomain and port
-  const target = `http://localhost:${targetPort}`;
-
-  // Proxy WebSocket upgrade requests
-  if (req.headers.upgrade && req.headers.upgrade.toLowerCase() === 'websocket') {
-    proxy.ws(req, res, { target });
-  } else {
-    // Proxy regular HTTP requests
-    proxy.web(req, res, { target });
-  }
+  const targetPort = getTargetPort(req);
+  if (targetPort === null) { // If targetPort is null, refuse the connection
+    res.statusCode = 403;
+    res.end('Forbidden');
+    return;
+  };
+  proxy.web(req, res, { target: `http://${targetHost}:${targetPort}` });
 });
 
-// Start the server on port 3000
-server.listen(3000, () => {
-  console.log('Proxy server listening on port 3000');
+server.on('upgrade', (req, socket, head) => { // Websocket upgrade event listener
+  const targetPort = getTargetPort(req);
+  proxy.ws(req, socket, head, { target: `ws://${targetHost}:${targetPort}` });
 });
 
-// Helper function to extract the subdomain from the request
-function getSubdomain(req) {
-  const host = req.headers.host;
-  const parts = host.split('.');
-  return parts.length > 2 ? parts[0] : '';
-}
-```
-
-In this updated code, the `targetPort` is set based on the subdomain extracted from the request's `host` header. The `target` URL is then constructed using `localhost` and the corresponding `targetPort`. Requests will be proxied to the appropriate port on `localhost` based on the subdomain.
-
-For example, if the subdomain is `app1.example.com`, requests will be forwarded to `localhost:8081`. If the subdomain is `app2.example.com`, requests will be forwarded to `localhost:8082`. For any other subdomain, requests will be forwarded to `localhost:8080`.
-
-Make sure to replace `/path/to/certificate.crt` and `/path/to/privatekey.key` with the actual paths to your SSL certificate and key files.
-
-With this modification, the proxy server will route requests to different ports on `localhost` based on the subdomain specified in the request's `host` header.
+server.listen(proxyPort, () => { // Start the server
+  console.log(`Proxy server listening on port ${proxyPort}`);
+});
